f446cc4c2974d2082f534948428611762f17b980
MatheusCBarreto <devmattbarreto@protonmail.com>
2024-10-12 22:50:32 -0300

    projeto pronto

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -1,10 +1,72 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import quizz_questions from '../../../assets/data/quizz_questions.json';  
 
 @Component({
   selector: 'app-quizz',
   templateUrl: './quizz.component.html',
   styleUrl: './quizz.component.css'
 })
-export class QuizzComponent {
+export class QuizzComponent implements OnInit {
+
+  title:string = 'CONTEÚDO TEMPORÁRIO';
+  
+  questions:any;
+  questionsSelected:any;
+
+  answers:string[] = [];
+  answersSelected:string = '';
+
+  questionIndex:number = 0;
+  questionMaxIndex:number = 0;
+
+  finished:boolean = false;
+
+  ngOnInit(): void {
+    if(quizz_questions) {
+      this.finished = false;
+      this.title = quizz_questions.title;
+
+      this.questions = quizz_questions.questions;
+      this.questionsSelected = this.questions[this.questionIndex];
+
+      this.questionIndex = 0;
+      this.questionMaxIndex = this.questions.length;
+    }
+  }
+
+  playerChoose(value:string) {
+    this.answers.push(value)
+    this.nextStep();
+  }
+  
+
+  async nextStep() {
+    this.questionIndex++;
+
+    if(this.questionMaxIndex > this.questionIndex) {
+      this.questionsSelected = this.questions[this.questionIndex];
+    } else {
+      const finalAnswer:string = await this.checkResult(this.answers);
+
+      this.finished = true;
+      this.answersSelected = quizz_questions.results[finalAnswer as keyof typeof quizz_questions.results];
+    }
+  }
+
+  async checkResult(answers:string[]) {
+
+    const result = answers.reduce((previous, current, i, arr) => {
+      if(
+        arr.filter(item => item === previous).length >
+        arr.filter(item => item === current).length
+      ){
+        return previous
+      } else {
+        return current
+      }
+    }) 
+
+    return result;
+  }
 
 }
